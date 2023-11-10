03872159b69e22643e268d049d913481ad4b51b2
imAlexDuh <4uva40k99@gmail.com>
2023-11-10 18:02:08 +0300

    something

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 const router = require('./routes/default');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000, URL = 'mongodb://127.0.0.1/mestodb' } = process.env;
 
@@ -13,10 +14,17 @@ app.use(express.json());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors);
 app.use(cookieParser());
 mongoose.connect(URL);
 app.use(requestLogger);
 app.use(router);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.use(errorLogger);
 app.use(errors());
