b48af28d246a89257c6a53c6f9e0ac033dd79911
imAlexDuh <4uva40k99@gmail.com>
2023-11-15 02:20:47 +0300

    fixie

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -8,14 +8,6 @@ const requestLogger = expressWinston.logger({
   format: winston.format.json(),
 });
 
-const errorLogger = expressWinston.errorLogger({
-  transports: [
-    new winston.transports.File({ filename: 'error.log' }),
-  ],
-  format: winston.format.json(),
-});
-
 module.exports = {
   requestLogger,
-  errorLogger,
 };
